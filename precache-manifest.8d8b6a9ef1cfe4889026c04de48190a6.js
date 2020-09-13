d01218572e0b3accb2456cc3a60bd8f5a8143289
shaishavkumar <shaishavkumar@users.noreply.github.com>
2020-09-13 13:28:34 +0530

    Updates

---
+++ precache-manifest.8d8b6a9ef1cfe4889026c04de48190a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e10c6485d49683390550206773df732e",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "812d5a6c9fc8cf1cfc2f",
+    "url": "/todo/static/css/main.e702defc.chunk.css"
+  },
+  {
+    "revision": "fedd7d47d4167496b59f",
+    "url": "/todo/static/js/2.d0c5b57d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo/static/js/2.d0c5b57d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "812d5a6c9fc8cf1cfc2f",
+    "url": "/todo/static/js/main.1f97c8ff.chunk.js"
+  },
+  {
+    "revision": "73306ce994a6ed0954a2",
+    "url": "/todo/static/js/runtime-main.a940b003.js"
+  }
+]);
